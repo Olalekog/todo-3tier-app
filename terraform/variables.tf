f5fb79da8b2a705e14ab9cd834fe6938a43e19b9
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-04 15:23:34 -0400

    Enabling Environment Promotion

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,120 +1,139 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS Region where resources will be deployed."
   type        = string
-  default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Project name used for resource naming"
+  description = "Project name used for naming and tagging AWS resources."
   type        = string
   default     = "react-js-application"
 }
 
 variable "environment" {
-  description = "Deployment environment name"
+  description = "Deployment environment name such as dev, uat, or prod."
   type        = string
-  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "uat", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, uat, prod."
+  }
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR block for the application VPC."
   type        = string
-  default     = "10.40.0.0/16"
-}
-
-variable "az_count" {
-  description = "Number of AZs/subnet groups to create"
-  type        = number
-  default     = 2
 }
 
 variable "public_subnet_cidrs" {
-  description = "Public subnet CIDRs for frontend and NAT Gateway"
+  description = "CIDR blocks for public subnets."
   type        = list(string)
-  default     = ["10.40.1.0/24", "10.40.2.0/24"]
 }
 
 variable "private_app_subnet_cidrs" {
-  description = "Private app subnet CIDRs for backend"
+  description = "CIDR blocks for private application subnets."
   type        = list(string)
-  default     = ["10.40.11.0/24", "10.40.12.0/24"]
 }
 
 variable "private_db_subnet_cidrs" {
-  description = "Private DB subnet CIDRs for RDS"
+  description = "CIDR blocks for private database subnets."
   type        = list(string)
-  default     = ["10.40.21.0/24", "10.40.22.0/24"]
 }
 
-variable "allowed_http_cidr" {
-  description = "CIDR allowed to access the frontend over HTTP"
-  type        = string
-  default     = "0.0.0.0/0"
+variable "availability_zones" {
+  description = "Availability Zones used by the VPC subnets."
+  type        = list(string)
 }
 
 variable "allowed_ssh_cidr" {
-  description = "CIDR allowed to SSH to EC2 instances. Set to your IP/32."
+  description = "CIDR block allowed to SSH into the public frontend EC2 instance. Use your public IP with /32."
   type        = string
-  default     = "0.0.0.0/0"
-}
 
-variable "key_name" {
-  description = "Optional EC2 key pair name for SSH access. Leave null to disable SSH key injection."
-  type        = string
-  default     = null
+  validation {
+    condition     = can(cidrhost(var.allowed_ssh_cidr, 0))
+    error_message = "allowed_ssh_cidr must be a valid CIDR block, for example 203.0.113.10/32."
+  }
 }
 
 variable "frontend_instance_type" {
-  description = "EC2 instance type for the frontend workload"
+  description = "EC2 instance type for the frontend workload."
   type        = string
   default     = "t3.micro"
 }
 
 variable "backend_instance_type" {
-  description = "EC2 instance type for the backend workload"
+  description = "EC2 instance type for the backend workload."
   type        = string
   default     = "t3.micro"
 }
 
-
-variable "image_tag" {
-  description = "Docker image tag deployed to EC2 instances."
+variable "ami_id" {
+  description = "Optional AMI ID for EC2 instances. Leave empty to use the latest Amazon Linux 2023 AMI from SSM."
   type        = string
-  default     = "latest"
+  default     = ""
+}
+
+variable "key_name" {
+  description = "Optional EC2 key pair name for SSH access. Leave empty if SSH key access is not required."
+  type        = string
+  default     = ""
 }
 
 variable "db_name" {
-  description = "RDS database name"
+  description = "Name of the application database."
   type        = string
-  default     = "tododb"
+  default     = "todoapp"
 }
 
 variable "db_username" {
-  description = "RDS master username"
+  description = "Master username for the RDS MySQL database."
   type        = string
   default     = "todo_admin"
 }
 
 variable "db_password" {
-  description = "RDS master password. Use terraform.tfvars locally or DB_PASSWORD GitHub secret in GitHub Actions."
+  description = "Master password for the RDS MySQL database. Pass this from GitHub Secrets."
   type        = string
   sensitive   = true
 }
 
 variable "db_instance_class" {
-  description = "RDS instance class"
+  description = "RDS DB instance class."
   type        = string
   default     = "db.t3.micro"
 }
 
 variable "db_allocated_storage" {
-  description = "RDS allocated storage in GB"
+  description = "Allocated storage size for RDS in GB."
   type        = number
   default     = 20
 }
 
-variable "tags" {
-  description = "Common tags"
+variable "db_engine_version" {
+  description = "MySQL engine version for RDS."
+  type        = string
+  default     = "8.0"
+}
+
+variable "image_tag" {
+  description = "Docker image tag to deploy from ECR. Usually latest or the Git commit SHA."
+  type        = string
+  default     = "latest"
+}
+
+variable "frontend_container_port" {
+  description = "Container port exposed by the frontend application."
+  type        = number
+  default     = 80
+}
+
+variable "backend_container_port" {
+  description = "Container port exposed by the backend application."
+  type        = number
+  default     = 8000
+}
+
+variable "common_tags" {
+  description = "Additional common tags applied to resources."
   type        = map(string)
   default     = {}
 }
