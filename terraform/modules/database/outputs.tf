7f0abdc46afba8aadb364763bd3ffeb00726deca
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-04 16:14:15 -0400

    Enabling Environment Promotion

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,3 +1,19 @@
-output "db_endpoint" { value = aws_db_instance.todo.endpoint }
-output "db_address" { value = aws_db_instance.todo.address }
-output "db_port" { value = aws_db_instance.todo.port }
+output "db_address" {
+  description = "RDS database endpoint address."
+  value       = aws_db_instance.this.address
+}
+
+output "db_endpoint" {
+  description = "RDS database endpoint with port."
+  value       = aws_db_instance.this.endpoint
+}
+
+output "db_port" {
+  description = "RDS database port."
+  value       = aws_db_instance.this.port
+}
+
+output "db_name" {
+  description = "Database name."
+  value       = aws_db_instance.this.db_name
+}
