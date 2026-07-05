f732c4a5c598bf5edb56f1464864fe2196fe6d27
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-04 23:29:34 -0400

    Updating WorkFlow

--- terraform/locals-image-uri-example.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  ecr_registry = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
-
-  frontend_image_uri = "${local.ecr_registry}/${var.project_name}/${var.environment}/todo-frontend:${var.frontend_image_tag}"
-  backend_image_uri  = "${local.ecr_registry}/${var.project_name}/${var.environment}/todo-backend:${var.backend_image_tag}"
-}
