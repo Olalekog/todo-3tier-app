9ce4b952e2668292fcfdb90f7896eb2ae031db5f
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-07 11:46:34 -0400

    Integrating Security Toolings

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -1,7 +1,24 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "this" {
   description             = var.description
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "AllowAccountRootFullAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
 
   tags = var.tags
 }
