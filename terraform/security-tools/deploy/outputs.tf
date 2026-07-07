74f3c4a63fc20f9c25df1df5dc53cfc0219b595a
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-07 17:01:14 -0400

    Integrating Security Toolings

--- terraform/security-tools/deploy/outputs.tf
+++ terraform/security-tools/deploy/outputs.tf
@@ -1,4 +1,4 @@
 output "sonarqube_url" {
-  description = "SonarQube URL (only set when enable_sonarqube = true)."
-  value       = var.enable_sonarqube ? "http://${module.sonarqube_compute[0].private_ip}:9000" : null
+  description = "Public SonarQube URL (only set when enable_sonarqube = true)."
+  value       = var.enable_sonarqube ? "http://${module.sonarqube_compute[0].public_ip}:9000" : null
 }
