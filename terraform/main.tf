1299fc63380511a76f3518095692fd87510f1733
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-08 08:43:11 -0400

    Updating SonarQube Infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,14 +38,14 @@ locals {
 module "network" {
   source = "./modules/network"
 
-  project_name              = var.project_name
-  environment               = var.environment
-  vpc_cidr                  = var.vpc_cidr
-  availability_zones        = local.azs
-  public_subnet_cidrs       = var.public_subnet_cidrs
-  private_app_subnet_cidrs  = var.private_app_subnet_cidrs
-  private_db_subnet_cidrs   = var.private_db_subnet_cidrs
-  tags                      = local.common_tags
+  project_name             = var.project_name
+  environment              = var.environment
+  vpc_cidr                 = var.vpc_cidr
+  availability_zones       = local.azs
+  public_subnet_cidrs      = var.public_subnet_cidrs
+  private_app_subnet_cidrs = var.private_app_subnet_cidrs
+  private_db_subnet_cidrs  = var.private_db_subnet_cidrs
+  tags                     = local.common_tags
 }
 
 module "ecr" {
