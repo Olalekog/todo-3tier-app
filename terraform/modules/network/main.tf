2a41eabfdb1d3ad5bf8ab6ad1c14abf780c414ee
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-09 22:30:21 -0400

    Deploying App Infrastructure

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -81,7 +81,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -119,13 +119,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private_app" {
-  count          = length(aws_subnet.private_app)
+  count          = length(var.private_app_subnet_cidrs)
   subnet_id      = aws_subnet.private_app[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_db" {
-  count          = length(aws_subnet.private_db)
+  count          = length(var.private_db_subnet_cidrs)
   subnet_id      = aws_subnet.private_db[count.index].id
   route_table_id = aws_route_table.private.id
 }
