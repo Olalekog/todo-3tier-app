54ed14c69914b3516db1c583634228731adc57ce
Olalekan Ogundare <flexcloud2019@gmail.com>
2026-07-10 08:25:44 -0400

    Suppress VPC flow log check

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,4 +1,4 @@
-#checkov:skip=CKV2_AWS_11:VPC flow logs are intentionally disabled for this environment per current deployment requirements.
+#checkov:skip=CKV2_AWS_11:VPC flow logs are intentionally disabled for this environment to avoid additional logging cost.
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
